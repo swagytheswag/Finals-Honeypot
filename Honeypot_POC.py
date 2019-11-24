1eee55ddf58090100c818b9d4d4d080b70eafc15
swagytheswag <mnm183127@gmail.com>
2019-11-24 11:46:55 +0200

    A small bug-fix

--- Honeypot_POC.py
+++ Honeypot_POC.py
@@ -4,10 +4,10 @@ asset_addr = "10.0.0.1"
 honeypot_addr = "10.0.0.13"
 
 def handle_packets(pkt):
-    pkt = IP(src=honeypot_addr, dst=asset_addr)/ICMP()/"response to "+str(pkt.summary())
+    pkt = IP(src=honeypot_addr, dst=asset_addr)/ICMP()/("response to "+str(pkt[0][Raw]))
     send(pkt)
 
 
 # Capture Every incoming traffic from the asset.
-
-sniff(prn=handle_packets, filter="icmp", store=0, count=1)
+while True:
+    sniff(prn=handle_packets, filter="icmp", store=0, count=1)
--- Test_POC.py
+++ Test_POC.py
@@ -2,6 +2,6 @@ from scapy.all import *
 
 asset_addr = "10.0.0.1"
 send(IP(src = '10.0.0.8', dst = asset_addr)/ICMP()/'LALALALALALA')
-pkt = sniff(filter="icmp", count=1)
+pkt = sniff(filter="icmp", count=1)[0]
 
-print pkt.show()
+pkt.show()
