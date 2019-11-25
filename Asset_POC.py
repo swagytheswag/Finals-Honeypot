10943dd48e2e5dbbbf1e8258d58ba36861fcfd85
swagytheswag <mnm183127@gmail.com>
2019-11-25 21:15:18 +0200

    Add files via upload
    Fully supports short TCP sessions.

--- Asset_POC.py
+++ Asset_POC.py
@@ -1,10 +1,13 @@
-import pydivert
 
-asset_addr = "10.0.0.1"
+import pydivert
+import scapy.all as scapy
+
+
+asset_addr = "10.0.0.5"
 honeypot_addr = "10.0.0.13"
 incoming_addr = []
 # Capture Every incoming traffic.
-w = pydivert.WinDivert("icmp and ip.DstAddr == %s"%(asset_addr))
+w = pydivert.WinDivert("tcp and (ip.SrcAddr == 10.0.0.6 or ip.SrcAddr == %s)"%(honeypot_addr))
 
 
 w.open()  # Packets will be captured from now on
@@ -18,14 +21,32 @@ while True:
         packet.ipv4.dst_addr = str(incoming_addr.pop(0))
         packet.direction = 0 # outbounding
         w.send(packet)
-        
+        # if it's TCP, send also a FIN
+        if packet.tcp:
+            # using scapy
+            scapy.send(scapy.IP(src=asset_addr,dst=packet.ipv4.dst_addr)\
+                /scapy.TCP(sport=packet.src_port,dport=packet.dst_port,flags='FA',\
+                    seq=packet.tcp.seq_num, ack=packet.tcp.ack_num))
+
+      
     # Else it's an outside traffic
     else:
-        # save the incoming 
-        incoming_addr.append(str(packet.ipv4.src_addr))
-        # rout the packet to the honeypot
-        packet.ipv4.dst_addr = honeypot_addr
-        packet.direction = 0 # outbounding
-        w.send(packet)
+        # if it's TCP SYN, Send back SYN-ACK (to the same port)
+        if packet.tcp.syn:
+            # using scapy
+            scapy.send(scapy.IP(src=asset_addr,dst=packet.ipv4.src_addr)\
+                /scapy.TCP(sport=packet.dst_port,dport=packet.src_port,flags='SA',\
+                    seq=packet.tcp.ack_num, ack=packet.tcp.seq_num+1))
+        # if it's TCP ACK, it's alright ;)
+        elif packet.tcp.ack and not packet.tcp.psh:
+            pass
+        
+        else:
+            # save the incoming 
+            incoming_addr.append(str(packet.ipv4.src_addr))
+            # rout the packet to the honeypot
+            packet.ipv4.dst_addr = honeypot_addr
+            packet.direction = 0 # outbounding
+            w.send(packet)
 
 w.close()  # stop capturing packets
--- Test_POC.py
+++ Test_POC.py
@@ -1,7 +1,27 @@
+"""
 from scapy.all import *
+import socket
 
-asset_addr = "10.0.0.1"
-send(IP(src = '10.0.0.8', dst = asset_addr)/ICMP()/'LALALALALALA')
-pkt = sniff(filter="icmp", count=1)[0]
+asset_addr = "10.0.0.5"
 
-pkt.show()
+send(IP(src = '10.0.0.6', dst = asset_addr)/TCP())#/'LALALALALALA')
+
+pkt = sniff(filter="ip.addr == 10.0.0.5 and tcp", count=1)
+
+
+print str(pkt.show())
+"""
+import socket
+import sys
+
+TCP_IP = '10.0.0.5'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print "received data:", data
