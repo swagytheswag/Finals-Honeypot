c20efd0a7b845566df435dcba1846e7fb19d8cb7
swagytheswag <mnm183127@gmail.com>
2019-12-16 21:51:00 +0200

    FOR THE HONEYPOT
    made the login vaunrable to SQLI
    non hashed passwords

---
+++ flaskblog/flaskblog/routes.py
@@ -0,0 +1,71 @@
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog import app, db
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+from sqlalchemy.sql import text
+
+posts = [
+    {
+        'author': 'Kfir Sharabi',
+        'title': 'My New Web Server!',
+        'content': 'This is my first Web Server! Welcome!',
+        'date_posted': 'december 11, 2019'
+    },
+    {
+        'author': 'Kfir Sharabi',
+        'title': 'My New Web Server!',
+        'content': 'This is my first Web Server! Welcome!',
+        'date_posted': 'december 11, 2019'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        password = form.password.data
+        user = User(username=form.username.data, email=form.email.data, password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in.', 'success')
+        return redirect(url_for('login'))
+    return render_template("register.html", title='Register', form=form)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.query(User).from_statement(
+            text('SELECT * FROM User where email = "'+form.email.data+'" AND password = "'+form.password.data+'"')).first()
+        if user:
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check the credentials again.\n', 'danger')
+    return render_template("login.html", title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title='Account')
