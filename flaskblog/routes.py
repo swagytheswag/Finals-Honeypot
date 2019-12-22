ae403389fbc204caced0e0d89b0d81a2f6134d06
swagytheswag <mnm183127@gmail.com>
2019-12-22 12:43:24 +0200

    Add files via upload

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,9 @@
 from flask import render_template, url_for, flash, redirect, request
-from flaskblog import app, db, bcrypt
+from flaskblog import app, db
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+from sqlalchemy.sql import text
 
 posts = [
     {
@@ -35,8 +36,8 @@ def register():
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        password = form.password.data
+        user = User(username=form.username.data, email=form.email.data, password=password)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in.', 'success')
@@ -49,13 +50,14 @@ def login():
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        user = db.session.query(User).from_statement(
+            text('SELECT * FROM User where email = "'+form.email.data+'" AND password = "'+form.password.data+'"')).first()
+        if user:
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check the credentials again.', 'danger')
+            flash('Login Unsuccessful. Please check the credentials again.\n', 'danger')
     return render_template("login.html", title='Login', form=form)
 
 @app.route("/logout")
