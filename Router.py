e42bf33192fc543ede2be8728c89f9f102c1d9e3
swagytheswag <mnm183127@gmail.com>
2020-02-06 16:10:30 +0200

    Add files via upload

--- Router.py
+++ Router.py
@@ -3,7 +3,9 @@ import scapy.all as scapy
 import re
 import urllib
 import logging
-from SessionManager import SessionManager, Client, Session
+#from SessionManager import SessionManager, Client, Session
+import socket
+import threading
 
 
 class Router(object):
@@ -15,8 +17,8 @@ class Router(object):
         """
         self.asset_addr = asset_addr
         self.honeypot_addr = honeypot_addr
-        self.session_manager = SessionManager()
-        self.w = pydivert.WinDivert("(tcp.SrcPort == 5000 or tcp.DstPort == 5000) and ip.SrcAddr != %s"%(self.asset_addr))
+        #self.session_manager = SessionManager()
+        self.w = pydivert.WinDivert("(tcp.SrcPort == 50000 or tcp.DstPort == 50000) and ip.SrcAddr != %s and ip.SrcAddr != %s"%(self.asset_addr, self.honeypot_addr))
 
         open('logger.log', 'w').close()
         self.logger = logging.getLogger(__name__)
@@ -33,122 +35,17 @@ class Router(object):
 
         self.logger.addHandler(self.file_handler)
         self.logger.addHandler(self.stream_handler)
-    
+
+
     def start_router(self):
         self.logger.info('Starting The Router')
         self.w.open()   # Packets will be captured from now on
+        threading.Thread(target = send_to_asset_handler, args = (self.asset_addr, )).start()
+        threading.Thread(target = send_to_honeypot_handler, args = (self.honeypot_addr, )).start()
 
     def stop_router(self):
         self.logger.info('Stopping The Router')
         self.w.close()  # stop capturing packets
-
-    def from_honeypot(self, packet):
-        return packet.ipv4.src_addr == self.honeypot_addr # checks if the packet from the honeypot
-    
-    def handle_packet_from_honeypot(self, packet):
-        # rout the packet back to the original sender
-        self.session_manager.update_honeypot_sessions(packet)
-        if self.session_manager.is_client_blacklisted(packet):
-            self.send_from_honeypot_to_client(packet)
-            self.logger.debug('Redirecting a packet from the Honeypot to the Hacker at %s' % (packet.ipv4.dst_addr))
-
-    def handle_packet_from_outside(self, packet):
-        """
-        Handles packets from outside clients.
-        :param packet:
-        """
-        if packet.ipv4.src_addr == self.asset_addr:
-            self.w.send(packet)
-        else:
-            self.session_manager.update_asset_sessions(packet)
-            if self.session_manager.is_client_blacklisted(packet):
-                pass
-                #self.logger.debug('Redirecting a blacklisted packet to the Honeypot from %s' % (packet.ipv4.src_addr))
-
-            # if the packet includes malicious data
-            elif self.is_malicious(packet):
-                self.session_manager.blacklist_client(packet)
-                self.logger.debug('Redirecting a malicious packet to the Honeypot from %s' % (packet.ipv4.src_addr))
-                #self.start_tcp_session_with_honeypot(packet)
-
-            # if the packet is safe, let it go
-            else:
-                self.logger.debug('Let in a non-malicious, non-blacklisted packet from %s' % (packet.ipv4.src_addr))
-                self.w.send(packet)
-
-            self.send_to_honeypot(packet)
-
-    def send_to_honeypot(self, packet):
-        """
-        Redirect the packet to the honeypot.
-        :param packet:
-        :return:
-        """
-        sess = self.session_manager.get_honeypot_session(packet)
-        packet.direction = 0  # outbounding
-        pkt = scapy.IP(packet.ipv4.raw.tobytes())
-        # Correct the IP/TCP fields
-        del pkt[scapy.IP].id
-        pkt[scapy.IP].id = sess.ipv4_id
-        del pkt[scapy.IP].chksum
-        pkt[scapy.IP].src = self.asset_addr
-        pkt[scapy.IP].dst = self.honeypot_addr
-        pkt[scapy.TCP].seq = sess.tcp_ack
-        pkt[scapy.TCP].ack = sess.tcp_seq + sess.tcp_header_len - 40
-
-            # Change the payload to fit the Honeypot's IP
-        payload_before = len(pkt[scapy.TCP].payload)
-        pkt[scapy.TCP].payload = scapy.Raw(str(pkt[scapy.TCP].payload).replace(self.asset_addr, self.honeypot_addr))
-        payload_after = len(pkt[scapy.TCP].payload)
-        payload_dif = payload_after - payload_before
-        pkt[scapy.IP].len = pkt[scapy.IP].len + payload_dif
-
-        # calculate the correct TCP fields
-        del pkt[scapy.TCP].chksum
-        del pkt[scapy.TCP].window
-        pkt[scapy.TCP] = pkt[scapy.TCP].__class__(str(pkt[scapy.TCP]))
-
-        scapy.send(pkt)
-
-    def send_from_honeypot_to_client(self, packet):
-        sess = self.session_manager.get_asset_session(packet)
-
-        packet.direction = 0  # outbounding
-        pkt = scapy.IP(packet.ipv4.raw.tobytes())
-        # Correct the IP/TCP fields
-        pkt[scapy.IP].id = sess['ipv4_id']
-        del pkt[scapy.IP].chksum
-        pkt[scapy.IP].src = self.asset_addr
-        pkt[scapy.IP].dst = sess['tcp_src_port']
-        pkt[scapy.TCP].seq = sess['tcp_ack']
-        pkt[scapy.TCP].ack = sess['tcp_seq'] + sess['tcp_header_len']
-
-        # Change the payload to fit the Honeypot's IP
-        payload_before = len(pkt[scapy.TCP].payload)
-        pkt[scapy.TCP].payload = scapy.Raw(str(pkt[scapy.TCP].payload).replace(self.asset_addr, self.honeypot_addr))
-        payload_after = len(pkt[scapy.TCP].payload)
-        payload_dif = payload_after - payload_before
-        pkt[scapy.IP].len = pkt[scapy.IP].len + payload_dif
-
-        # calculate the correct TCP fields
-        del pkt[scapy.TCP].chksum
-        del pkt[scapy.TCP].window
-        pkt[scapy.TCP] = pkt[scapy.TCP].__class__(str(pkt[scapy.TCP]))
-
-        scapy.send(pkt)
-
-    def router_mainloop(self):
-        '''
-        The mainloop of the Router
-        '''
-        packet = self.w.recv()  # Read a single packet
-        # If the packet came from the honeypot
-        if self.from_honeypot(packet):
-            self.handle_packet_from_honeypot(packet)
-        
-        # Else it's an outside traffic
-        else:
-            self.handle_packet_from_outside(packet)
 
     def is_malicious(self, packet):
         """
@@ -164,47 +61,107 @@ class Router(object):
             credentials = (urllib.unquote(m.group('email')), urllib.unquote(m.group('password')))
             for cred in credentials:
                 if '"' in cred or "'" in cred:
-                    self.logger.warning('SQLInjection attempt caught from %s'%(packet.ipv4.src_addr))
+                    self.logger.warning('SQLInjection attempt caught from %s' % (packet.ipv4.src_addr))
                     return True
         return False
 
-    def start_tcp_session_with_honeypot(self, packet):
-        synack = scapy.sr1(scapy.IP(src=self.asset_addr, dst=self.honeypot_addr) \
-                   / scapy.TCP(sport=packet.src_port, dport=packet.dst_port, flags='S', \
-                               seq=packet.tcp.seq_num-1, ack=packet.tcp.ack_num))
+    def router_mainloop(self):
+        '''
+        The mainloop of the Router
+        '''
 
-        scapy.send(scapy.IP(src=self.asset_addr, dst=self.honeypot_addr) \
-                   / scapy.TCP(sport=packet.src_port, dport=packet.dst_port, flags='A', \
-                               seq=packet.tcp.seq_num, ack=synack[scapy.TCP].seq+1))
+        global send_to_asset
+        global send_to_honeypot
 
-        packet.direction = 0  # outbounding
-        pkt = scapy.IP(packet.ipv4.raw.tobytes())
-        # Correct the IP/TCP fields
-        del pkt[scapy.IP].id
-        del pkt[scapy.IP].chksum
-        pkt[scapy.IP].src = self.asset_addr
-        pkt[scapy.IP].dst = self.honeypot_addr
-        pkt[scapy.TCP].seq = synack[scapy.TCP].ack
-        pkt[scapy.TCP].ack = synack[scapy.TCP].seq + 1
+        packet = self.w.recv()  # Read a single packet
 
-        # Change the payload to fit the Honeypot's IP
-        payload_before = len(pkt[scapy.TCP].payload)
-        pkt[scapy.TCP].payload = scapy.Raw(str(pkt[scapy.TCP].payload).replace(self.asset_addr, self.honeypot_addr))
-        payload_after = len(pkt[scapy.TCP].payload)
-        payload_dif = payload_after - payload_before
-        pkt[scapy.IP].len = pkt[scapy.IP].len + payload_dif
+        # if it's TCP SYN, Send back SYN-ACK (to the same port)
+        if packet.tcp.syn:
+            # using scapy
+            scapy.send(scapy.IP(src=self.asset_addr, dst=packet.ipv4.src_addr) \
+                       / scapy.TCP(sport=packet.dst_port, dport=packet.src_port, flags='SA', \
+                                   seq=packet.tcp.ack_num, ack=packet.tcp.seq_num + 1))
+        # if it's TCP ACK, it's alright ;)
+        elif packet.tcp.ack and not packet.tcp.psh:
+            pass
 
-        # calculate the correct TCP fields
-        del pkt[scapy.TCP].chksum
-        del pkt[scapy.TCP].window
-        pkt[scapy.TCP] = pkt[scapy.TCP].__class__(str(pkt[scapy.TCP]))
-
-        scapy.send(pkt)
+        elif self.is_malicious(packet):
+            send_to_honeypot.append(packet)
+        else:
+            send_to_asset.append(packet)
 
 
+def get_packet_payload(packet):
+    pkt = scapy.IP(packet.ipv4.raw.tobytes())
+    return str(pkt[scapy.TCP].payload)
+
+def send_to_asset_handler(asset_addr):
+    global send_to_asset
+    while True:
+        if send_to_asset:
+            packet = send_to_asset.pop(0)
+            payload = get_packet_payload(packet)
+
+            HOST = asset_addr
+            PORT = 55555
+            BUFFSIZ = 4096
+            ADDR = (HOST, PORT)
+
+            tcpClientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcpClientSock.connect(ADDR)
+            tcpClientSock.send(payload)
+
+            payloads_to_send = []
+            while '</html>' not in payloads_to_send[-1]:
+                payloads_to_send.append(tcpClientSock.recv(BUFFSIZ))
+
+            tcpClientSock.close()
+
+            for payload_to_send in payloads_to_send:
+                scapy.send(scapy.IP(src=packet.ipv4.dst_addr, dst=packet.ipv4.src_addr) \
+                           / scapy.TCP(sport=packet.dst_port, dport=packet.src_port, flags='PA', \
+                                       seq=packet.tcp.ack_num, ack=packet.tcp.seq_num + 1) \
+                           / scapy.Raw(payload_to_send))
+
+            scapy.send(scapy.IP(src=packet.ipv4.dst_addr, dst=packet.ipv4.src_addr) \
+                       / scapy.TCP(sport=packet.dst_port, dport=packet.src_port, flags='F', \
+                                   seq=packet.tcp.ack_num, ack=packet.tcp.seq_num + 1))
 
 
-router = Router("10.0.0.7", "10.0.0.17")    # Initialize the router opbject
+def send_to_honeypot_handler(honeypot_addr):
+    global send_to_honeypot
+    while True:
+        if send_to_honeypot:
+            packet = send_to_asset.pop(0)
+            payload = get_packet_payload(packet)
+
+            HOST = honeypot_addr
+            PORT = 55555
+            BUFFSIZ = 4096
+            ADDR = (HOST, PORT)
+
+            tcpClientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcpClientSock.connect(ADDR)
+            tcpClientSock.send(payload)
+            payload_to_send = tcpClientSock.recv(BUFFSIZ)
+            tcpClientSock.close()
+
+            scapy.send(scapy.IP(src=packet.ipv4.dst_addr, dst=packet.ipv4.src_addr) \
+                       / scapy.TCP(sport=packet.dst_port, dport=packet.src_port, flags='PA', \
+                                   seq=packet.tcp.ack_num, ack=packet.tcp.seq_num + 1) \
+                       / scapy.Raw(payload_to_send))
+
+            scapy.send(scapy.IP(src=packet.ipv4.dst_addr, dst=packet.ipv4.src_addr) \
+                       / scapy.TCP(sport=packet.dst_port, dport=packet.src_port, flags='F', \
+                                   seq=packet.tcp.ack_num, ack=packet.tcp.seq_num + 1))
+
+
+global send_to_asset
+send_to_asset = []
+global send_to_honeypot
+send_to_honeypot = []
+
+router = Router("172.16.10.189", "172.16.10.179")    # Initialize the router object
 router.start_router()  # Packets will be captured from now on
 while True:
     router.router_mainloop()
