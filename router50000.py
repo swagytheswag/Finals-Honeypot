b4448e0cd3676cae91659a7613ee69ea6f812b74
swagytheswag <mnm183127@gmail.com>
2020-05-08 14:38:44 +0300

    Made it a bit better :)
    can upload images and better end-of-session :)

--- router50000.py
+++ router50000.py
@@ -76,8 +76,16 @@ class Router50000(BaseWrapper):
                            / scapy.TCP(sport=packet.dst_port, dport=packet.src_port, flags='FA', \
                                        seq=packet.tcp.ack_num, ack=packet.tcp.seq_num), \
                            verbose=False)
+                scapy.send(scapy.IP(src=self.asset_addr, dst=packet.ipv4.src_addr) \
+                           / scapy.TCP(sport=packet.dst_port, dport=packet.src_port, flags='A', \
+                                       seq=packet.tcp.ack_num, ack=packet.tcp.seq_num+1), \
+                           verbose=False)
             # if packet has payload over TCP, deal with it
             elif packet.tcp.ack and len(self.get_packet_payload(packet)) > 1:
+                scapy.send(scapy.IP(src=self.asset_addr, dst=packet.ipv4.src_addr) \
+                           / scapy.TCP(sport=packet.dst_port, dport=packet.src_port, flags='A', \
+                                       seq=packet.tcp.ack_num, ack=packet.tcp.seq_num+len(self.get_packet_payload(packet))), \
+                           verbose=False)
                 payload = self.get_packet_payload(packet)
                 self.all_packets.append((packet, payload))
             # if it's TCP ACK, it's alright ;)
@@ -105,10 +113,10 @@ class Router50000(BaseWrapper):
                 m = re.search(pattern, payload)
                 content_length = m.group('content_length') if m else 0
 
-                pattern = re.compile(r"\r\n\r\n(?P<content>.*)")
+                pattern = re.compile(r"\r\n\r\n(?P<content>(.|\s)*)")
                 m = re.search(pattern, payload)
                 # if not, search for the full payload in the next packets
-                while not m or int(len(m.group('content').encode('utf-8'))) != int(content_length):
+                while not m or int(len(m.group('content'))) != int(content_length):
                     while True:
                         if self.all_packets: break
                     packet, xpayload = self.all_packets.pop(0)
@@ -150,11 +158,12 @@ class Router50000(BaseWrapper):
 
                 ack_back = scapy.IP(packet.ipv4.raw.tobytes())
                 ack_back[scapy.TCP].seq = ack_back[scapy.TCP].seq + len(ack_back[scapy.TCP].payload)
-
+                '''
                 scapy.send(scapy.IP(src=ack_back[scapy.IP].dst, dst=ack_back[scapy.IP].src) \
                            / scapy.TCP(sport=ack_back[scapy.TCP].dport, dport=ack_back[scapy.TCP].sport, flags='A', \
                                        seq=ack_back[scapy.TCP].ack, ack=ack_back[scapy.TCP].seq), \
                            verbose=False)
+                '''
                 n = int(len(payload_to_send)/5)
                 payloads_to_send = [payload_to_send[0:n], payload_to_send[n:2*n], payload_to_send[2*n:3*n], payload_to_send[3*n:4*n], payload_to_send[4*n:]]
                 for pay in payloads_to_send:
