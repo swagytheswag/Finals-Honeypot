09492c7dc0ebc2e9d902083a8636b00ea5d9d34b
swagytheswag <mnm183127@gmail.com>
2020-05-09 21:06:38 +0300

    Update start_routers.py

--- start_routers.py
+++ start_routers.py
@@ -5,7 +5,7 @@ from father_router import FatherRouter
 
 father_router = FatherRouter("10.0.0.8", "10.0.0.9") # Initialize the router object
 
-#router50000 = Router50000(father_router)
-#router50000.start_router()
+router50000 = Router50000(father_router)
+router50000.start_router()
 router40000 = Router40000(father_router)
 router40000.start_router()
